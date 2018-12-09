bc505499a089a9bb482ee5d00808af635b9b66ff
Lyall Beveridge <37994809+senornosketchy@users.noreply.github.com>
2018-12-09 18:38:32 +1100

    Cleaned up code

--- MainMotionScript.py
+++ MainMotionScript.py
@@ -1,48 +1,42 @@
-# This program will allow the snapshot function from the fswebcam to be triggered by the sensor
+# A simple program designed to take a webcam picture when a PIR sensor is activated
+# 9APR18
+# By Lyall Beveridge
 
 import onionGpio
 import time
 import os
 import sys
 
-# set up the GPIO pins
+# -- Set up the GPIO pins
 
-# set up the LED
-ledPin = 19							# Set the pin
-led = onionGpio.OnionGpio(ledPin)	# Instantiate the pin as a GPIO object
-led.setOutputDirection()			# Set direction
+# Set up the LED
+ledPin = 19								# Set the pin
+led = onionGpio.OnionGpio(ledPin)		# Instantiate the pin as a GPIO object
+led.setOutputDirection()				# Set direction
 
-# set up the PIR Sensor
-sensePin = 11						# Set the pin
+# Set up the PIR Sensor
+sensePin = 11							# Set the pin
 sensor = onionGpio.OnionGpio(sensePin)	# Instantiate the pin as a GPIO object
-# sensor._freeGpio()					# Free the sensor first
-sensor.setInputDirection()			# Set direction
+sensor.setInputDirection()				# Set direction
 
-lastSnap = time.time()				# set up the timer
-lastPir = 0;						# track the timer
-pir = 0;							# test the comparison operator
-
+lastSnap = time.time()					# Set up the timer
 
 try:
 	while 1:
-		pir = sensor.getValue()		# Get the sensor value
-		
-		
+		pir = sensor.getValue()			# Get the sensor value
 		elapsed = time.time() - lastSnap	# Get the time difference
 		# If the sensor is high and the timer has elapsed past 30 seconds
 		if (int(pir) == 1 and int(elapsed) >= 20):
-			print "Taking snapshot!"
-			
 			# Take a snapshot
+			print "Taking snapshot!"
 			os.system('fswebcam -r 1280x720 /tmp/mounts/SD-P1/sd_snapshots/home_test1/"%Y-%m-%d_%H%M%S".jpg');
-			lastSnap = time.time()	# Restart the timer
+			lastSnap = time.time()		# Restart the timer
 			
-		led.setValue(pir)			# Light up the LED if the sensor is high
-		print "pir state: " + str(pir) + "lastPir state: " + str(lastPir) + "elapsed: " + str(elapsed) + "\n"
-		
-		lastPir = pir
+		led.setValue(pir)				# Light up the LED if the sensor is high
+		# print "pir state: " + str(pir) + "lastPir state: " + str(lastPir) + "elapsed: " + str(elapsed) + "\n"
 		time.sleep(1)
 	
-except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-    # led._freeGpio()
-    sensor._freeGpio()	# cleanup all GPIO
+except KeyboardInterrupt:				# If CTRL+C is pressed, exit cleanly:
+    sensor._freeGpio()					# Cleanup sensor GPIO
+    sys.exit()							# Exit cleanly
+	
